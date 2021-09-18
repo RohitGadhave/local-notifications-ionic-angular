11ea32979537e493401705b73b790cb31f92aaa8
rohit Gadhave <rohitgadhave249@gmail.com>
2021-09-18 15:47:09 +0530

    home button added for custom notification

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 
+
+import { Platform } from '@ionic/angular';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+public model:any={};
+  constructor(public platform:Platform,private localNotifications: LocalNotifications) {}
+  localNotification(){
+    // Schedule delayed notification
+    let trigger:any= { every: "minute", count: 5 }
+    this.localNotifications.schedule({
+      id:1,
+      title: 'Local ILocalNotification Example',
+      text: this.model.message?this.model.message:'Delayed ILocalNotification',
+      //trigger: {at: new Date(new Date().getTime() + 3600)},
+      
+      led: 'FF0000',
+      sound: null
+    });
+  }
 }
